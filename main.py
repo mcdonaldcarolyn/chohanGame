44441e159610152858c82d726e458b059925f79e
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-05 13:41:18 +0000

    Create rules and start main game loop

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import random, sys
+
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN', 4: 'SHI', 5: 'GO', 6: 'ROKU'}
+
+print(''' Cho-Han by inventwithpython.com. In this traditional Japanese game dice game, two dice are rolled in a bamboo cup by the dealer sitting on the floor, the player must guess if the dice total is an even (cho) or odd (hand)''')
+
+purse = 5000 
+
+while True: 
+    print('you have', purse, 'mon. How much do you bet? (or Quit)')
+    while True 
+        
